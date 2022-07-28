22874fd1d5369bef5ff3bd3e3230691c347c78f8
Oleh Nikulin <oleh.nikulin.dev@gmail.com>
2022-07-28 12:49:00 +0300

    2nd task is done

--- JS_events/index.js
+++ JS_events/index.js
@@ -21,11 +21,54 @@ $('[data-toggle=popover]').hover(function (e) {
   }
 });
 
-//scroll-bar
+//TASK 1 scroll-bar
 const bar = document.querySelector('.scroll-bar');
 window.addEventListener('scroll', () => {
   const sсroll = document.body.scrollTop || document.documentElement.scrollTop;
   const windowHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   let scrollProgress = (sсroll / windowHeight) * 100;
   bar.style.width = scrollProgress + '%';
+});
+
+//TASK 2 form validation
+const userSettingsFormInput = document.querySelectorAll('#userSettings .form-control');
+const submitForm = document.querySelector('#submitSettings');
+const userDataSettings = {
+  username: '',
+  email: '',
+  firstName: '',
+  lastName: '',
+};
+
+userSettingsFormInput.forEach((input) =>
+  input.addEventListener('change', (e) => {
+    switch (e.target.getAttribute('id')) {
+      case 'inputUserName':
+        userDataSettings.username = e.target.value;
+        break;
+      case 'inputUserEmail':
+        userDataSettings.email = e.target.value;
+        break;
+      case 'inputUserFirstName':
+        userDataSettings.firstName = e.target.value;
+        break;
+      case 'inputUserLastName':
+        userDataSettings.lastName = e.target.value;
+        break;
+    }
+  })
+);
+
+submitForm.addEventListener('click', () => {
+  let isShow = true;
+  for (data in userDataSettings) {
+    if (!userDataSettings[data]) {
+      isShow = false;
+      alert(`enter data: ${data}`);
+    }
+  }
+
+  if (isShow) {
+    window.open('about:blank').alert(`${JSON.stringify(userDataSettings)}`);
+  }
 });
